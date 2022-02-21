9eb2c5423c74d942831a586a5054550568a83738
aurelienbrethes <aurelienbrethes@gmail.com>
2022-02-21 18:45:05 +0100

    create cartContext + cart component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import NavBar from 'components/header/NavBar';
 import AboutUs from 'components/pages/aboutus/AboutUs';
+import Cart from 'components/pages/Cart/Cart';
 import Collections from 'components/pages/collections/Collections';
 import Contact from 'components/pages/contact/Contact';
 import Home from 'components/pages/home/Home';
@@ -8,6 +9,8 @@ import ProfileSettings from 'components/pages/Profile/ProfileSettings';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import { CartContextProvider } from './contexts/CartContext';
+
 const routes = [
   { path: '/home', component: <Home /> },
   { path: '/collections', component: <Collections /> },
@@ -15,17 +18,20 @@ const routes = [
   { path: '/contact', component: <Contact /> },
   { path: '/profile', component: <Profile /> },
   { path: '/profile/settings', component: <ProfileSettings /> },
+  { path: '/cart', component: <Cart /> },
 ];
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Routes>
-        {routes.map((route, index) => (
-          <Route key={index} path={route.path} element={route.component} />
-        ))}
-      </Routes>
+      <CartContextProvider>
+        <Routes>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.component} />
+          ))}
+        </Routes>
+      </CartContextProvider>
     </div>
   );
 }
---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,29 @@
+import { Shirts } from 'graphQl/generated';
+import React, { createContext, useState } from 'react';
+
+interface AppContextInterface {
+  itemCart: Shirts[];
+  setItemCart: React.Dispatch<React.SetStateAction<Shirts[]>>;
+}
+
+const CartContext = createContext<AppContextInterface>({
+  itemCart: [],
+  setItemCart: () => {},
+});
+
+export default CartContext;
+
+type Props = { children: React.ReactNode };
+export const CartContextProvider: React.FC<Props> = ({ children }) => {
+  const [itemCart, setItemCart] = useState<Shirts[]>([]);
+
+  return (
+    <CartContext.Provider
+      value={{
+        itemCart,
+        setItemCart,
+      }}>
+      {children}
+    </CartContext.Provider>
+  );
+};

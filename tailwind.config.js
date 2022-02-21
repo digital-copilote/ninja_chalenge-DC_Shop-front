f6f940269e712290f9fe98bd49dca78ef4144e43
Jimmy <jimmy.ganci@gmail.com>
2022-02-21 18:41:04 +0100

    navBar: added color and deleted box-shadow and switch searchBar to link

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: ['./src/**/*.{jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#5BB89C',
+        },
+        secondary: {
+          DEFAULT: '#F6F6F6',
+        },
+        text: {
+          DEFAULT: '#444444',
+        },
+      },
+      fontFamily: {
+        inter: ['Inter', 'Arial', 'sans-serif'],
+        montserrat: ['Montserrat', 'Arial', 'sans-serif'],
+        chakra: ['Chakra Petch', 'sans-serif'],
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 };

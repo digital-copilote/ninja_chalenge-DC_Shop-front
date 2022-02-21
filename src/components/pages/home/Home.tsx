5e8bd2977c6e04d9d4e0dcae25d765656140d835
Jimmy <jimmy.ganci@gmail.com>
2022-02-21 17:24:41 +0100

    App.tsx: made route; NavBar.tsx: begin navbar and navigation

---
+++ src/components/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import ShirtContainer from 'components/Shirt/ShirtContainer';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <ShirtContainer />
+    </div>
+  );
+};
+
+export default Home;

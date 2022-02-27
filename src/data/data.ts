2b1895ef77648c6a8d5cf529261dd63417d08179
Jimmy <jimmy.ganci@gmail.com>
2022-02-27 18:53:19 +0100

    create section component with button component

---
+++ src/data/data.ts
@@ -0,0 +1,28 @@
+export const dataSection = [
+  {
+    id: 1,
+    title: 'Nos Collections',
+    description: 'Découvrez toutes nos collections!',
+    titleButton: 'Voir nos collections',
+  },
+  {
+    id: 2,
+    title: 'Nos Artistes',
+    description:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    titleButton: "Voir plus d'artistes",
+  },
+  {
+    id: 3,
+    title: 'Nos Associations',
+    description:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    titleButton: 'Voir nos assos',
+  },
+  {
+    id: 3,
+    title: 'Nos Engagements',
+    description: "Lorem Ipsum has bery's standard dummy text ever since the 1500s.",
+    titleButton: 'En savoir plus',
+  },
+];
